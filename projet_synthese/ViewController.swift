2f5d06571187bcd97e389bb0c9097dc756f6be64
alexandre <toto@me.com>
2017-11-13 09:05:23 -0500

    Premier commit

---
+++ projet_synthese/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projet_synthese
+//
+//  Created by Etudiant on 17-11-13.
+//  Copyright © 2017 Etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
