3ebaa2dd26ff88e204745090956cbabb5a4b5572
Susyane <xcode@cstj.qc.ca>
2017-11-20 09:37:36 -0500

    Mise à jour du design

--- projet_synthese/ViewController.swift
+++ projet_synthese/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+//import AVKit
 
 class ViewController: UIViewController {
-
+    /*@IBAction func debutVideo(_ sender: Any)
+    {
+        if let path = Bundle.main.path(forResource: "video", ofType: "mp4")  //accès au url de la vidéo
+        {
+            let video = AVPlayer(url:URL(fileURLWithPath: path)) //accès à la vidéo elle-même
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion: {
+                video.play()
+            })
+        }
+    }*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
