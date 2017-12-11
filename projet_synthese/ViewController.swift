c809bef77fe30634f6c1647e966f7da543294c8a
Alexandre <a@hotmail.com>
2017-12-11 13:30:58 -0500

    Floaty button done !

--- projet_synthese/ViewController.swift
+++ projet_synthese/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import AVKit
 import AVFoundation
+import Floaty
 
  
- class ViewController: UIViewController, UITableViewDataSource {
+ class ViewController: UIViewController, UITableViewDataSource, FloatyDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        return donneesPays?.Response.count ?? 0
        
@@ -31,6 +32,7 @@ import AVFoundation
     private var selectionDeBase = 0
     var donneesPays: Pays?
     //var audioPlayer: AVAudioPlayer?
+    
     
     
     @IBOutlet weak var CVpays: UITableView!
@@ -69,12 +71,37 @@ import AVFoundation
             }
         }*/
         print("i'm master")
+        layoutFAB()
         donneesDePays()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
+    }
+    
+    func layoutFAB() {
+        let fab = Floaty()
+        let item = FloatyItem()
+        item.buttonColor = UIColor(red: 188/255, green: 46/255, blue: 35/255, alpha: 1)
+        item.circleShadowColor = UIColor.red
+        item.titleShadowColor = UIColor(red: 188/255, green: 46/255, blue: 35/255, alpha: 0.5)
+        item.title = "Custom item"
+        item.handler = { item in
+            
+        }
+        
+        fab.addItem(title: "Menu"){item in self.dismiss(animated: true, completion: nil)}
+        
+        fab.addItem("Become an Admin", icon: UIImage(named: "Contacts Filled.png")) { item in
+            let alert = UIAlertController(title: "Become an Admin", message: "Send a request to be an admin and be able to write notices", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Send Request", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        fab.fabDelegate = self
+        
+        self.view.addSubview(fab)
+        
     }
  
     //MARK:- Obtension de données
